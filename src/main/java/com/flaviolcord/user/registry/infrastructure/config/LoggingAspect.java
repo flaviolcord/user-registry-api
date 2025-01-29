66c97179504bfd34b036d05c921890461e4c0353
flaviolcord <flaviolucas197@gmail.com>
2025-01-29 01:06:35 +0100

    complete javadoc comments

--- src/main/java/com/flaviolcord/user/registry/infrastructure/config/LoggingAspect.java
+++ src/main/java/com/flaviolcord/user/registry/infrastructure/config/LoggingAspect.java
@@ -8,6 +8,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+/**
+ * Aspect for logging method calls and their execution details in the application.
+ * <p>
+ * This class intercepts method executions within the specified package (com.flaviolcord.user.registry)
+ * and logs the method's entry, arguments, result, and execution time. It also logs any exceptions thrown
+ * during the method execution.
+ * </p>
+ * <p>
+ * Sensitive data in method arguments and results are sanitized before logging to ensure privacy and security.
+ * </p>
+ */
 @Aspect
 @Component
 @AllArgsConstructor
@@ -15,10 +26,30 @@ public class LoggingAspect {
 
     private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
+    /**
+     * Pointcut expression that matches all method executions within the
+     * {@code com.flaviolcord.user.registry} package.
+     * <p>
+     * This pointcut is used to define where the logging aspect should be applied.
+     * </p>
+     */
     @Pointcut("execution(* com.flaviolcord.user.registry..*(..))")
     public void applicationPackagePointcut() {
     }
 
+    /**
+     * Around advice that intercepts method executions matched by the {@link #applicationPackagePointcut()}
+     * pointcut expression. Logs the method entry, arguments, result, and execution time. Also logs exceptions
+     * if they are thrown.
+     * <p>
+     * The method execution is wrapped around to log the entry and exit times as well as any exceptions thrown
+     * during execution. The result is also sanitized before being logged.
+     * </p>
+     *
+     * @param joinPoint the join point that provides access to method details and arguments
+     * @return the result of the method execution
+     * @throws Throwable if the method execution throws an exception, it will be rethrown after logging
+     */
     @Around("applicationPackagePointcut()")
     public Object logAroundMethod(ProceedingJoinPoint joinPoint) throws Throwable {
         long startTime = System.currentTimeMillis();
--- src/main/java/com/flaviolcord/user/registry/infrastructure/mapper/UserMapper.java
+++ src/main/java/com/flaviolcord/user/registry/infrastructure/mapper/UserMapper.java
@@ -5,14 +5,49 @@ import com.flaviolcord.user.registry.domain.model.User;
 import com.flaviolcord.user.registry.infrastructure.persistence.UserEntity;
 import org.mapstruct.Mapper;
 
+/**
+ * Mapper interface for converting between different representations of a user.
+ * <p>
+ * This interface provides methods to map between:
+ * <ul>
+ *     <li>Domain Model (User) and DTO (UserDTO)</li>
+ *     <li>Domain Model (User) and JPA Entity (UserEntity)</li>
+ * </ul>
+ * The component model is set to "spring" for integration with Spring's dependency injection.
+ * </p>
+ */
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    // Domain Model <-> DTO
+    /**
+     * Converts a UserDTO to a User domain model.
+     *
+     * @param userDTO the UserDTO to convert
+     * @return the corresponding User domain model
+     */
     User toDomainModel(UserDTO userDTO);
+
+    /**
+     * Converts a User domain model to a UserDTO.
+     *
+     * @param user the User domain model to convert
+     * @return the corresponding UserDTO
+     */
     UserDTO toDTO(User user);
 
-    // Domain Model <-> JPA Entity
+    /**
+     * Converts a User domain model to a UserEntity for persistence in the database.
+     *
+     * @param user the User domain model to convert
+     * @return the corresponding UserEntity
+     */
     UserEntity toEntity(User user);
+
+    /**
+     * Converts a UserEntity (database representation) to a User domain model.
+     *
+     * @param userEntity the UserEntity to convert
+     * @return the corresponding User domain model
+     */
     User toDomainModel(UserEntity userEntity);
 }
